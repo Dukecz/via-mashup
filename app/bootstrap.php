eb9a60958fc0a3ad47c94eeb51cd0c022e24e831
Dukecz <stukov0210@gmail.com>
2016-01-04 21:49:43 +0100

    updated debugger ip

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = new Nette\Configurator;
 
-$configurator->setDebugMode('192.168.1.1'); // enable for your remote IP
+$configurator->setDebugMode('109.81.209.230'); // enable for your remote IP
 $configurator->enableDebugger(__DIR__ . '/../log');
 
 $configurator->setTempDirectory(__DIR__ . '/../temp');
